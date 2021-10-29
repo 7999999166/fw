8c8e0b99f52075a0297596f804ce49ceeae8a2ec
Kanik Goyal <acrotechllp@gmail.com>
2021-10-30 02:24:10 +0530

    jknj

---
+++ src/pages/Sample.svelte
@@ -0,0 +1,81 @@
+<style>
+.aa{
+    display: flex;
+    flex-direction: column;
+    margin-top: 102px;
+}
+.bb{
+    height: 25vh;
+    display: flex;
+    flex-wrap: wrap;
+
+}
+.cc{
+    height: 35px;
+    width: 100px;
+    margin-right: 20px;
+}
+.dd{
+    margin-right: 30px;
+    height: 35px;
+    padding-top: 5px;
+    color: white;
+}
+.ee{
+    background-color: #212529;
+    width: 100vw;
+}
+.ff{
+    margin-top: 10px;
+}
+.gg{
+    width: 100vw;
+   
+    
+}
+.mm{
+    height: 35px;
+    margin-right: 30px;
+    padding-top: 5px;
+}
+.jj{
+    margin-bottom: 70px;
+}
+</style>
+<div class="aa">
+<div class="bb ee">
+    <h4 class="dd ff">Sample 1</h4>
+    <input class="w3-input w3-border cc w3-round ff" type="number" value="0" >
+    <button type="button" class="btn btn-warning cc ff">Apply</button>
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Ni" >
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Cr">
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Mn">
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Cu">
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="C">
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Others">   
+</div>
+<div class="bb gg">
+    <h4 class="ff mm">Sample 2</h4>
+    <input class="w3-input w3-border cc w3-round ff" type="number" value="0">
+    <button type="button" class="btn btn-warning cc ff">Apply</button> 
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Ni">
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Cr" >
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Mn" >
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Cu" >
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="C" >
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Others">  
+</div>
+<div class="bb ee jj">
+    <h4 class="dd ff">Sample 3</h4>
+    <input class="w3-input w3-border cc w3-round ff" type="number" value="0" >
+    <button type="button" class="btn btn-warning cc ff">Apply</button>
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Ni">
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Cr">
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Mn">
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="CU">
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="C">
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Others">   
+</div>
+
+
+</div>
