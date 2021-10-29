b6e6c6b02879c7c5cfadf4859b4987a56fd8a146
Kanik Goyal <acrotechllp@gmail.com>
2021-10-29 14:25:16 +0530

    jas

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -26,9 +26,9 @@
                 <th class="cc">Quantity</th>
                 <th class="cc dd">(req.)</th>
                 <th class="cc dd">1800</th>
-                
                 <th class="cc dd">945</th>
                 <th class="cc dd">Report</th>
+                <th class="cc dd">Short</th>
                 
             </tr>
         </thead>
@@ -37,9 +37,9 @@
                 <th class="cc">Elements</th>
                 <th class="cc dd">%</th>
                 <th class="cc dd">K.g</th>
-                
                 <th class="cc dd">K.g</th>
                 <th class="cc">%</th>
+                <th class="cc dd">K.g</th>
                 
             </tr>
         </thead>
@@ -48,54 +48,53 @@
                 <td>Ni</td>
                 <td>2</td>
                 <td>2400</td>
-               
                 <td>120</td>
                 <td class="ee">1.4</td>
+                <td>120</td>
                 
             </tr>
             <tr class="bb dd">
                 <td>Cr</td>
-                <td>Cell 2</td>
-                <td>Cell 3</td>
-               
-                <td>Cell 3</td>
-                <td class="ee">Cell 3</td>
+                <td>12.5</td>
+                <td>340</td>
+                <td>10</td>
+                <td class="ee">5</td>
+                <td>10</td>
                
             </tr>
             <tr class="bb dd">
                 <td>Mn</td>
-                <td>Cell 2</td>
-                <td>Cell 3</td>
-                
-                <td>Cell 1</td>
-                <td class="ee">Cell 3</td>
+                <td>9</td>
+                <td>1400</td>   
+                <td>4.5</td>
+                <td class="ee">900</td>
+                 <td>4.5</td>
                
             </tr>
             <tr class="bb dd">
                 <td>Cu</td>
-                <td>Cell 2</td>
-                <td>Cell 3</td>
-                
-                <td>Cell 5</td>
-                <td class="ee">Cell 3</td>
-               
+                <td>2</td>
+                <td>150</td>
+                <td>.75</td>
+                <td class="ee">60</td>
+                <td>.75</td>
             </tr>
             <tr class="bb dd">
                 <td>C</td>
-                <td>Cell 2</td>
-                <td>Cell 3</td>
-               
-                <td>Cell 5</td>
-                <td class="ee">Cell 3</td>
+                <td>.16</td>
+                <td>2.4</td>
+                <td>2.1</td>
+                <td class="ee">3.5</td>
+                <td>2.1</td>
                 
             </tr>
             <tr class="bb dd">
                 <td>Others</td>
-                <td>Cell 2</td>
-                <td>Cell 3</td>
-               
-                <td>Cell 5</td>
-                <td class="ee">Cell 3</td>
+                <td>75</td>
+                <td>1500</td>
+                <td>1000</td>
+                <td class="ee">73</td>
+                <td>500</td>
                
             </tr>
         </tbody>
